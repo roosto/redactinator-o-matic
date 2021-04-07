bbe07c2f1e3d33cceaa2d329f1aa67e8d404f6b0
Dustin Goldman <dustin@rubiconmd.com>
2021-04-07 14:07:41 -0400

    [WIP] add testing diff step to watcher.bash POC

--- watcher.bash
+++ watcher.bash
@@ -12,3 +12,15 @@ echo "write to 'now file at $(date)" > $TEMP_FILE_PREV
 
 echo prev file: $TEMP_FILE_PREV
 echo  now file: $TEMP_FILE_NOW
+
+watch_dir=~/sw/redactinator-o-matic/sample-images
+
+find $watch_dir -type f | egrep -e '/[a-f0-9]{32}.' > $TEMP_FILE_PREV
+
+echo -n '' > $TEMP_FILE_PREV # DEBUG: make diff show all files as new
+
+find $watch_dir -type f | egrep -e '/[a-f0-9]{32}.' > $TEMP_FILE_NOW
+
+diff $TEMP_FILE_PREV $TEMP_FILE_NOW
+
+
