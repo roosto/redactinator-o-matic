73efbe8493ab81b7f0230d844fd186b3b0264997
Dustin Goldman <dustin@rubiconmd.com>
2021-05-07 09:42:43 -0400

    make watch_dir a CLI arg instead of hardcoded

--- watcher.bash
+++ watcher.bash
@@ -6,21 +6,13 @@ ME="$(basename $0)"
 TEMP_FILE_PREV="$(mktemp -t "$ME")" || exit 1
 TEMP_FILE_NOW="$(mktemp -t "$ME")" || exit 1
 
-
-# echo "write to 'prev' file at $(date)" > $TEMP_FILE_PREV
-# echo "write to 'now file at $(date)" > $TEMP_FILE_PREV
-
-echo prev file: $TEMP_FILE_PREV
-echo  now file: $TEMP_FILE_NOW
-
-watch_dir=~/sw/redactinator-o-matic/sample-images
+watch_dir="$1"
 
 find $watch_dir -type f | egrep -e '/[a-f0-9]{32}.' > $TEMP_FILE_PREV
 
 echo -n '' > $TEMP_FILE_PREV # DEBUG: make diff show all files as new
 
 find $watch_dir -type f | egrep -e '/[a-f0-9]{32}.' > $TEMP_FILE_NOW
-
 
 diff $TEMP_FILE_PREV $TEMP_FILE_NOW \
 	| grep '^> ' \
