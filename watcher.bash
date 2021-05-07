31ab25e98581e29efcd52f6fcd50e247b23eedff
Dustin Goldman <dustin@rubiconmd.com>
2021-05-07 09:33:41 -0400

    add diffing and parsing of output as args to redaction script

--- watcher.bash
+++ watcher.bash
@@ -7,8 +7,8 @@ TEMP_FILE_PREV="$(mktemp -t "$ME")" || exit 1
 TEMP_FILE_NOW="$(mktemp -t "$ME")" || exit 1
 
 
-echo "write to 'prev' file at $(date)" > $TEMP_FILE_PREV
-echo "write to 'now file at $(date)" > $TEMP_FILE_PREV
+# echo "write to 'prev' file at $(date)" > $TEMP_FILE_PREV
+# echo "write to 'now file at $(date)" > $TEMP_FILE_PREV
 
 echo prev file: $TEMP_FILE_PREV
 echo  now file: $TEMP_FILE_NOW
@@ -21,6 +21,10 @@ echo -n '' > $TEMP_FILE_PREV # DEBUG: make diff show all files as new
 
 find $watch_dir -type f | egrep -e '/[a-f0-9]{32}.' > $TEMP_FILE_NOW
 
-diff $TEMP_FILE_PREV $TEMP_FILE_NOW
 
+diff $TEMP_FILE_PREV $TEMP_FILE_NOW \
+	| grep '^> ' \
+	| tee diff.out \
+	| sed -e 's/^> //g' \
+	| xargs ./redact-image.bash
 
